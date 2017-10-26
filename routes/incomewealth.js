85ad61f26962badd93bc0d22c81c7ae274ae7e03
baranbartu <bbartu.demirci@gmail.com>
2017-10-27 01:59:25 +0300

    made similar response structure for top10

--- routes/incomewealth.js
+++ routes/incomewealth.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET top10 income metrics. */
 router.get('/top10', function(req, res, next) {
-	connection.query('SELECT * from app_incomewealth',
+  var init = req.query.init;
+  var end = req.query.end;
+	connection.query(`SELECT income_top10, wealth_top10 from app_incomewealth WHERE year >= ${init} and year <= ${end}`,
     function (error, results, fields) {
 	  	if (error) {
 	  		res.send(JSON.stringify(
@@ -13,9 +15,9 @@ router.get('/top10', function(req, res, next) {
         // error section with 500 status
 	  	} else {
   			res.send(JSON.stringify(
-          {"status": 200, "error": null, "response": results})
+          {'data': results})
         );
-  			//If there is no error, all is good and response is 200OK.
+  			// If there is no error, all is good and response is 200OK.
 	  	}
   	});
 });
