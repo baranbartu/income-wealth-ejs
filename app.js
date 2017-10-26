d803f9e4393ea14d3348072a9f9bd9b457a5f3c6
baranbartu <bbartu.demirci@gmail.com>
2017-10-27 01:42:40 +0300

    mysql connection, started to implement first endpoint

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var incomewealth = require('./routes/incomewealth');
 
 var app = express();
 
@@ -22,8 +23,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Database connection
+app.use(function(req, res, next){
+	global.connection = mysql.createConnection({
+	  	host     : 'localhost',
+	  	user     : 'incomewealth',
+  		database : 'incomewealth',
+      password : '8e5HLr7gWas='
+	});
+	connection.connect();
+	next();
+});
+
 app.use('/', index);
-app.use('/users', users);
+app.use('/api/v1', incomewealth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
