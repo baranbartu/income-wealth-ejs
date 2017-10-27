b5a50ce31bb7c343c90703778ca56861f7743dba
baranbartu <bbartu.demirci@gmail.com>
2017-10-27 14:54:30 +0300

    added new vim bindings to be able write quickly

--- routes/incomewealth.js
+++ routes/incomewealth.js
@@ -5,21 +5,23 @@ var router = express.Router();
 router.get('/top10', function(req, res, next) {
   var init = req.query.init;
   var end = req.query.end;
-	connection.query(`SELECT income_top10, wealth_top10 from app_incomewealth WHERE year >= ${init} and year <= ${end}`,
-    function (error, results, fields) {
-	  	if (error) {
-	  		res.send(JSON.stringify(
-          {"status": 500, "error": error, "response": null}
-        )); 
-	  		// If there is error, we send the error in the 
+  connection.query(`SELECT income_top10, wealth_top10 from app_incomewealth WHERE year >= ${init} and year <= ${end}`,
+    function(error, results, fields) {
+      if (error) {
+        res.send(JSON.stringify({
+          "status": 500,
+          "error": error,
+          "response": null
+        }));
+        // If there is error, we send the error in the 
         // error section with 500 status
-	  	} else {
-  			res.send(JSON.stringify(
-          {'data': results})
-        );
-  			// If there is no error, all is good and response is 200OK.
-	  	}
-  	});
+      } else {
+        res.send(JSON.stringify({
+          'data': results
+        }));
+        // If there is no error, all is good and response is 200OK.
+      }
+    });
 });
 
 module.exports = router;
